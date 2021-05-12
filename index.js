65dadd8f08e4a3af1f2f7f5695da8295f00923c2
LABMATT <matt42764@gmail.com>
2021-05-13 01:24:06 +1000

    Added comments
    - Users manger

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// List of active users form spots.
 var activeUsers = [];
 
+// Host the website using express
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// On init connection then add the socket id to the array.
 io.on('connection', (socket) => {
   console.log('A new user with id of: ' + socket.id);
 
@@ -19,10 +22,12 @@ io.on('connection', (socket) => {
   console.log(activeUsers[0].id);
 });
 
+// Listen on that prot for the users.
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
+// An object that stores a users info.
 var userobj = function(userid) {
   this.id = userid;
   this.username = null;
