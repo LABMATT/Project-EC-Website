a4afe9c19c5ba5b2d7125a5c54c80d5209d10606
LABMATT <matt42764@gmail.com>
2021-05-13 01:16:33 +1000

    User Connection

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+var activeUsers = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('A new user with id of: ' + socket.id);
+
+  activeUsers.push(new userobj(socket.id));
+
+  console.log(activeUsers[0].id);
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
+var userobj = function(userid) {
+  this.id = userid;
+  this.username = null;
+  this.password = null;
+  this.projectdir = null;
+}
