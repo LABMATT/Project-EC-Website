09c15bb5a4f47afab7cce1b446a8fbe1783d43f5
LABMATT <matt42764@gmail.com>
2021-06-13 01:52:13 +1000

    Start fromualting the idea of how to make a file tree

--- fileExplore.js
+++ fileExplore.js
@@ -1,3 +1,5 @@
+var locations = [];
+
 var ftree = {
     root: ""
 }
@@ -8,3 +10,29 @@ function setRoot(newroot)
     document.getElementById("root").innerHTML = ftree.root;
 }
 
+
+// Idea is that the path is a path from the root egg /matt/styles/style.css
+// Program would split at slashes, make a new folder called styles, then put a file named styles.css in it.
+// If path was say /matt/styles/ then it would add an empty folder.
+// IF path was say /matt/styles then it would asume its a file and add a new file. 
+function newLocation(path) {
+
+    updateTree();
+}
+
+function removeLocation(path) {
+
+    updateTree();
+}
+
+// Is called when a change occures to the tree struture.
+function updateTree()
+{
+
+    locations.forEach(loc => {
+        if(loc.endsWith("/"))
+        {
+            
+        }
+    });
+}
