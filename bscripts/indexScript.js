e6d38d8569e0bed9d3e219f8c01bb16646d39613
LABMATT <matt42764@gmail.com>
2021-07-27 00:20:52 +1000

    Cleaning
    - Cleaning of index file in prep for php conversion

---
+++ bscripts/indexScript.js
@@ -0,0 +1,68 @@
+document.addEventListener('keydown', checkKey);
+
+function checkKey(e)
+{
+
+  console.log("key down is:" + e.code);
+  var keycode = e.code;
+
+  if(keycode == "Enter")
+  {
+    var focus = document.activeElement.id;
+    if(focus == "uname")
+    {
+
+      document.getElementById("psw").focus();
+    } else if(focus == "psw")
+    {
+      
+      login();
+    }
+  }
+}
+
+// Commits to longin and loads correct page.
+socket.on('login', function(msg){
+if(msg[0] == true)
+{
+  document.getElementById("er").style.color = "BLACK";
+  document.getElementById("er").innerHTML = "Loging you in!";
+  if(msg[1] == true)
+  {
+    window.location.href = "admin.html";
+  }
+  else{
+    window.location.href = "user.html";
+  }
+  
+} else {
+  document.getElementById("er").style.color = "RED";
+  document.getElementById("er").innerHTML = "Error loging you in. Maybe check your user name or password.";
+}
+});
+
+// Saves ID cookie.
+socket.on('id', function(msg){
+document.cookie = "echid=" + msg + ";";
+});
+
+// once login is clicked, get the data from the boxes and sed it to the server.
+function login()
+{
+var name = document.getElementById("uname").value;
+var pw = document.getElementById("psw").value;
+
+console.log("login clicked wiht " + name + " " + pw);
+
+socket.emit("login", [name, pw]);
+}
+
+// If socket fails to connect then alert the user the sever appears to be offline.
+socket.on('connect_error', function(err) {
+
+document.getElementById("stat").innerHTML = "Server Status:🔴Offline.";
+});
+
+socket.on("connect", function(){
+document.getElementById("stat").innerHTML = "Server Status:🟢Online.";
+});
---
+++ scripts/feFolder.js
@@ -0,0 +1,19 @@
+class feFolder {
+
+    constructor()
+    {
+      this.foldername = "";
+      this.path = "";
+      this.fileArray = [];
+    }
+
+    getFiles()
+    {
+        return this.fileArray;
+    }
+
+    getFolderName()
+    {
+        return this.foldername;
+    }
+}
